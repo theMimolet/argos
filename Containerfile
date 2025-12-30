72d8d3531fcf1f44fb1c9ce6bccc0b2f1521e864
Mimolet <themimolet@proton.me>
2025-12-30 23:09:00 +0100

    fix: god help me

--- Containerfile
+++ Containerfile
@@ -5,11 +5,11 @@ COPY build_files /
 # Base Image
 FROM ghcr.io/ublue-os/aurora:stable
 
-COPY --from=ctx /build.sh /tmp/build.sh
-RUN chmod +x /tmp/build.sh && \
-    /tmp/build.sh && \
-    rpm-ostree initramfs --enable && \
-    rm /tmp/build.sh
+RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    /ctx/build.sh
     
 ### LINTING
 ## Verify final image and contents are correct.
--- build_files/build.sh
+++ build_files/build.sh
@@ -8,26 +8,27 @@ echo ">>> Setting up the repository... <<<"
 wget https://pkg.surfacelinux.com/fedora/linux-surface.repo -O /etc/yum.repos.d/linux-surface.repo
 sed -i 's|$releasever|42|g' /etc/yum.repos.d/linux-surface.repo
 
+echo ">>> Cleaning up version locks <<<"
+
+# We must remove these first or they will block the swap with version requirements
+dnf5 -y remove kernel-devel-matched kernel-uki-virt kmod-xone
+
 echo ">>> Swapping Kernels <<<"
 
 # 2. Kernel Swap
-dnf5 -y swap kernel kernel-surface
-dnf5 -y swap kernel-core kernel-surface-core
-dnf5 -y swap kernel-modules kernel-surface-modules
-dnf5 -y swap kernel-modules-extra kernel-surface-modules-extra
-dnf5 -y swap kernel-devel kernel-surface-devel
-dnf5 -y swap libwacom libwacom-surface
-dnf5 -y swap libwacom-data libwacom-surface-data
+dnf5 -y --allow-lower-level swap kernel kernel-surface \
+    --replace "kernel-core=kernel-surface-core" \
+    --replace "kernel-modules=kernel-surface-modules" \
+    --replace "kernel-modules-extra=kernel-surface-modules-extra" \
+    --replace "kernel-devel=kernel-surface-devel" \
+    --replace "libwacom=libwacom-surface" \
+    --replace "libwacom-data=libwacom-surface-data"
 
 dnf5 -y install \
     kernel-surface-headers \
     iptsd \
     surface-control \
     surface-secureboot \
-
-dnf5 -y remove \
-    kernel-uki-virt \
-    kmod-xone
 
 echo ">>> Installing additionnal packages... <<<"
 
